35e9f9fc2432bbbc03482d226dccacb4f27ec696
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2021-10-14 00:51:45 +0800

    Create extract_reference_matrix_200.sh

---
+++ Test/extract_reference_matrix_200.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES=0 python extract_feature_matrix.py \
+      --file_list list_files/references \
+      --image_dir /dev/shm/references_images \
+      --o features/references_byol_200.hdf5 \
+      --model 50_matrix  --GeM_p 3 --checkpoint baseline_matrix_baro_c24.pth.tar --imsize 200
