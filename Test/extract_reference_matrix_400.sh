5e00a8a9c05113a0c1686538fe8b8575711861be
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2021-10-14 00:50:37 +0800

    Update extract_reference_matrix_400.sh

--- Test/extract_reference_matrix_400.sh
+++ Test/extract_reference_matrix_400.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python extract_feature_matrix.py \
       --file_list list_files/references \
-      --image_dir /dev/shm/reference_images \
+      --image_dir /dev/shm/references_images \
       --o features/reference_byol_400.hdf5 \
       --model 50_matrix  --GeM_p 3 --checkpoint baseline_matrix_baro_c24.pth.tar --imsize 400
