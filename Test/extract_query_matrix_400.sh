ce01e29773bdd98fa3d5fb21cebdc5c793b9e44d
Wenhao Wang <53076320+WangWenhao0716@users.noreply.github.com>
2021-10-14 00:55:32 +0800

    Create extract_query_matrix_400.sh

---
+++ Test/extract_query_matrix_400.sh
@@ -0,0 +1,5 @@
+CUDA_VISIBLE_DEVICES=0 python extract_feature_matrix.py \
+      --file_list list_files/dev_queries \
+      --image_dir /dev/shm/query_images_detection \
+      --o features/query_byol_400_detection.hdf5 \
+      --model 50_matrix  --GeM_p 3 --checkpoint baseline_matrix_baro_c24.pth.tar --imsize 400
